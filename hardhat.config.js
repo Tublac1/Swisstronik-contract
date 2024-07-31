7a64ce69436f183efda1b25ac966cebc2aeb7f9a
Tublac1 <finaldrop12@gmail.com>
2024-07-31 02:40:07 +0000

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [""], //Your private key starting with "0x"
+    },
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
+
+  await contract.waitForDeployment();
+
+  console.log(`Swisstronik contract deployed to ${contract.target}`);
+}
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
